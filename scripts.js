099250c7d283ca93bc60ddceb4839cd993b20a3e
Casey Cross <hellocaseycross@gmail.com>
2016-09-05 17:01:57 -0600

    Header is functional, added preliminary styling

--- scripts.js
+++ scripts.js
@@ -1,32 +1,34 @@
+var $ = jQuery.noConflict();
+
 $(document).ready(function() {
 
   $("#navToggle a").click(function(event) {
     event.preventDefault();
 
-    $(".main-header > nav").slideToggle();
+    $(".main-nav").slideToggle();
     $("#logo").toggleClass("menuUp menuDown");
   });
 
-  $(".main-header > nav > ul > li > a").click(function(event) {
+  $(".main-list-item > a").click(function(event) {
     if($( window ).width() <= "600") {
         if($(this).siblings().size() > 0 ) {
 		     event.preventDefault();
-            $(this).siblings().slideToggle("fast")
-            $(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
+            $(this).siblings().slideToggle("fast");
+            // $(this).children(".toggle").html($(this).children(".toggle").html() === 'close' ? 'expand' : 'close');
         }
     }
   });
 
   $(window).resize(function() {
       if($( window ).width() >= "600") {
-          $("header > nav").css("display", "block");
+          $(".main-nav").css("display", "block");
 
-          if($("#logo").attr('class') == "menuDown") {
+          if($("#logo").attr('class') === "menuDown") {
               $("#logo").toggleClass("menuUp menuDown");
           }
       }
       else {
-          $("header > nav").css("display", "none");
+          $(".main-nav").css("display", "none");
       }
   });
 
