3b93c083b9dc06e50f68811f0a324e13cb8b6065
Casey Cross <hellocaseycross@gmail.com>
2016-09-05 21:59:29 -0600

    More things

--- scripts.js
+++ scripts.js
@@ -11,25 +11,21 @@ $(document).ready(function() {
 
   $(".main-list-item > a").click(function(event) {
     if($( window ).width() <= "600") {
-        // if($(this).siblings().size() > 0 ) {
-		     event.preventDefault();
-            $(this).siblings().slideToggle("fast");
-            // $(this).children(".toggle").html($(this).children(".toggle").html() === 'close' ? 'expand' : 'close');
-        // }
+      event.preventDefault();
+      $(this).siblings().slideToggle("fast");
     }
   });
 
   $(window).resize(function() {
-      if($( window ).width() >= "600") {
-          $(".main-nav").css("display", "block");
+    if($( window ).width() >= "600") {
+      $(".main-nav").css("display", "block");
 
-          if($("#logo").attr('class') === "menuDown") {
-              $("#logo").toggleClass("menuUp menuDown");
-          }
+      if($("#logo").attr('class') === "menuDown") {
+        $("#logo").toggleClass("menuUp menuDown");
       }
-      else {
-          $(".main-nav").css("display", "none");
-      }
+    }
+    else {
+      $(".main-nav").css("display", "none");
+    }
   });
-
 });
